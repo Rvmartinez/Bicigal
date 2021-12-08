0822c7fd45ebdc2f10dc8cbb4601f0f635c0c802
sominho <csmartinez1996@gmail.com>
2021-12-08 18:57:38 +0100

    Creada únicamente la vista para editar usuario; falta desarrollo

---
+++ app/src/main/java/com/esei/bicigal/UserEditActivity.java
@@ -0,0 +1,14 @@
+package com.esei.bicigal;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserEditActivity extends AppCompatActivity {
+@Override
+    protected  void onCreate(Bundle savedInstanceState){
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.user_edit_activity);
+
+}
+}
