25020447ca93d6e5b4200e2186cfaf96e1d73f84
sominho <csmartinez1996@gmail.com>
2021-12-09 01:26:54 +0100

    Cambios interfaz edit

--- app/src/main/java/com/esei/bicigal/BikeShowCurrent.java
+++ app/src/main/java/com/esei/bicigal/BikeShowCurrent.java
@@ -16,24 +16,16 @@ public class BikeShowCurrent extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bikeshow);
-        // int[] imageSource;
-        //int[] bikeImages=getIntent().getExtras().putIntArray("fotoBici");
-        //String dates=savedInstanceState.getString("fechas");
         String dates=getIntent().getExtras().getString("fechas");
-        //int pos=savedInstanceState.getInt("posicion");
         String bikename=getIntent().getExtras().getString("nombre");
-        //imageSource = new int[]{R.drawable.ncm_prague,R.drawable.montana_trial,R.drawable.decathlon_basica};
-        //int imageSelection=imageSource[posicion];
 
         TextView bike=findViewById(R.id.id_bicicleta);
         bike.setText(bikename);
 
-        // TextView km=findViewById(R.id.id_km);
-
         TextView fecha=findViewById(R.id.id_fecha);
         fecha.setText(dates);
 
-        // Button btAtras=this.findViewById(R.id.bt_atras);
+
 
 
 
